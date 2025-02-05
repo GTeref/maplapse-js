876fb30064e48f6a1ad567befab670725838b90d
GTeref <giovanni.terefi@stonybrook.edu>
2025-02-05 11:53:33 -0500

    added support for language proficiency dataset

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const raceOptions = document.getElementById('race-options')
     const raceSelect = document.getElementById('race-select')
     const hispSelect = document.getElementById('hispanic-select')
+    const langSelect=document.getElementById('language-select')
 
     const mapPlaceholder = document.getElementById('map-placeholder')
 
@@ -21,7 +22,8 @@ document.addEventListener('DOMContentLoaded', function() {
         currentYear: 2019,
         mapData: null,
         selectedRace: '1',
-        selectedHispanic: '0'
+        selectedHispanic: '0',
+        selectedLanguage: '2'
     }
 
     raceSelect.disabled = true;
@@ -37,6 +39,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
     raceSelect.addEventListener('change', handleRaceChange)
     hispSelect.addEventListener('change', handleHispanicChange)
+    langSelect.addEventListener('change', handleLangChange)
 
     //toggleRaceOptions()
 
@@ -58,11 +61,42 @@ document.addEventListener('DOMContentLoaded', function() {
 
     function handleDatasetChange(e) {
         state.selectedDataset = e.target.value
-        toggleRaceOptions()
+        //toggleRaceOptions()
 
-        const isRaceDataset = state.selectedDataset === 'Race and Ethnicity'
-        raceSelect.disabled = !isRaceDataset
-        hispSelect.disabled = !isRaceDataset
+        const raceOptionsDiv=document.getElementById('race-options')
+        const langOptionsDiv=document.getElementById('language-options')
+        
+        // if Race and Ethnicity selected, hide language select dropdown
+        if (state.selectedDataset==='Race and Ethnicity') {
+            raceOptionsDiv.classList.add('visible')
+            langOptionsDiv.classList.remove('visible')
+            raceSelect.disabled=false
+            hispSelect.disabled=false
+            langSelect.disabled=true
+
+            timeSlider.min='2015'
+            timeSlider.max='2019'
+            timeSlider.value='2019'
+            state.currentYear=2019
+            yearDisplay.textContent=2019
+        } else if (state.selectedDataset==='Language Proficiency') {
+            raceOptionsDiv.classList.remove('visible')
+            langOptionsDiv.classList.add('visible')
+            raceSelect.disabled=true
+            hispSelect.disabled=true
+            langSelect.disabled=false
+            
+            // i just realized the ACS language dataset only has one year....
+            timeSlider.min='2013'
+            timeSlider.max='2013'
+            timeSlider.value='2013'
+            state.currentYear=2013
+            yearDisplay.textContent=2013
+        }
+
+        // const isRaceDataset = state.selectedDataset === 'Race and Ethnicity'
+        // raceSelect.disabled = !isRaceDataset
+        // hispSelect.disabled = !isRaceDataset
 
         if (state.selectedDataset) {
             mapPlaceholder.style.display = 'none'
@@ -89,6 +123,12 @@ document.addEventListener('DOMContentLoaded', function() {
         fetchAndUpdateMap()
     }
 
+    function handleLangChange(e) {
+        state.selectedLanguage=e.target.value
+        fetchAndUpdateMap()
+    }
+    
+
     async function fetchAndUpdateMap() {
         if (!state.selectedDataset) {
             console.log("no dataset selected; map will not render")
@@ -99,27 +139,46 @@ document.addEventListener('DOMContentLoaded', function() {
             dataset: state.selectedDataset,
             race: state.selectedRace,
             hispanic: state.selectedHispanic,
+            language: state.selectedLanguage,
             year: state.currentYear
         });
 
         // fetch data from the dropdown menus to feed into url
         const hispParam = state.selectedDataset === 'Race and Ethnicity' ? `&HISP=${state.selectedHispanic}` : '';
         const raceParam = state.selectedDataset === 'Race and Ethnicity' ? `&RACE=${state.selectedRace}` : '';
+        const langParam=state.selectedDataset==='Language Proficiency' ? `&LAN7=${state.selectedLanguage}`: ''
 
-        const nameField = state.currentYear <= 2018 ? 'GEONAME' : 'NAME'
-
-        let requestURL = `https://api.census.gov/data/${state.currentYear}/pep/charagegroups?get=${nameField},POP${raceParam}${hispParam}&for=state:*`
+        let requestURL;
+        if (state.selectedDataset==='Race and Ethnicity') {
+            const nameField = state.currentYear <= 2018 ? 'GEONAME' : 'NAME'
+            requestURL= `https://api.census.gov/data/${state.currentYear}/pep/charagegroups?get=${nameField},POP${raceParam}${hispParam}&for=state:*`
+        } else if (state.selectedDataset==='Language Proficiency') {
+            requestURL = `https://api.census.gov/data/2013/language?get=EST,LANLABEL,NAME&for=state:*${langParam}`
+        }
+        
 
         const response = await fetch(requestURL)
         console.log("Fetching from:", requestURL)
         const json = await response.json();
-        const rows = json.slice(1).map(row => ({
-            state: row[4],
-            NAME: row[0],
-            POP: row[1],
-            RACE: row[2],
-            HISP: row[3]
-        }));
+        const rows = json.slice(1).map(row => {
+            if (state.selectedDataset ==='Race and Ethnicity') {
+                return {
+                    state: row[4],
+                    NAME: row[0],
+                    POP: row[1],
+                    RACE: row[2],
+                    HISP: row[3]
+                }
+            } else {
+                return {
+                    state: row[4],
+                    NAME: row[2],
+                    POP: row[0],
+                    LANG: row[1]
+                }
+            }
+            
+        });
 
         function unpack(rows, key) {
             return rows.map(row => row[key]);
@@ -149,7 +208,14 @@ document.addEventListener('DOMContentLoaded', function() {
             locations: unpack(rows, 'state').map(code => codeToState[code]),
             z: unpack(rows, 'POP').map(Number),
             text: unpack(rows, 'NAME'),
-            autocolorscale: true
+            colorscale: state.selectedDataset === 'Language Proficiency' ? [
+                [0, 'rgb(240, 247, 255)'],  // light blue
+                [1, 'rgb(31, 119, 180)']    // dark blue
+            ] : [
+                [0, 'rgb(255, 245, 240)'],  // light red
+                [1, 'rgb(103, 0, 13)']      // dark red
+            ]
+            //autocolorscale: true
             // locations: ['CA', 'TX', 'NY'], // Example states
             // z: [10000000, 20000000, 15000000], // Example population values
             // text: ['California', 'Texas', 'New York'],
@@ -170,10 +236,19 @@ document.addEventListener('DOMContentLoaded', function() {
             '1': 'Hispanic'
         };
 
-        let title = `${state.currentYear} US Population`;
-        if (state.selectedDataset === 'Race and Ethnicity') {
-            title += ` - ${raceLabels[state.selectedRace]} (${hispanicLabels[state.selectedHispanic]})`;
-        }
+        const languageLabels = {
+            '1': 'Population 5 years and over',
+            '2': 'Speak only English',
+            '3': 'Speak a language other than English at home',
+            '4': 'Spanish and Spanish Creole', // this doesn't work for some reason
+            '5': 'Other Indo-European Languages',
+            '6': 'Asian and Pacific Island Languages',
+            '7': 'All Other Languages'
+        };
+
+        let title = state.selectedDataset === 'Race and Ethnicity' 
+        ? `${state.currentYear} US Population - ${raceLabels[state.selectedRace]} (${hispanicLabels[state.selectedHispanic]})`
+        : `2013 US Population - ${languageLabels[state.selectedLanguage]}`;
 
         var layout = {
             title: title,
