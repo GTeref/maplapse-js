1180f35a85429087581f5c04fa93c008240c3ae3
GTeref <giovanni.terefi@stonybrook.edu>
2025-09-12 15:12:39 -0400

    More migration work

--- components/app.js
+++ components/app.js
@@ -1,4 +1,6 @@
+import { on } from "nodemon";
 import * as db from "../util/db.mjs";
+import { MapController } from "./map";
 
 window.mapApp() = () => ({
 
@@ -31,12 +33,20 @@ window.mapApp() = () => ({
         lucide.createIcons()
 
         // add map and sidebar controller later
+        this.mapController = new MapController(this);
+        this.setupWatchers();
 
 
-    }
+    },
 
     setupWatchers(){
-        this.$watch('selectedDataset', ()=> )
+        this.$watch('selectedDataset', ()=> this.onDatasetChange())
+        this.$watch('currentYear', ()=> this.onYearChange())
+        this.$watch('selectedRace', ()=> this.onRaceChange())
+        this.$watch('selectedHispanic', ()=> this.onHispanicChange())
+        this.$watch('selectedLanguage', ()=> this.onLanguageChange())
+        this.$watch('mapStyle', ()=> this.onMapStyleChange())
+    },
     
     //event handlers
     onDatasetChange(){
@@ -44,6 +54,73 @@ window.mapApp() = () => ({
             this.mapController?.fetchAndUpdateMap()
             
         }
-    }
+
+        
+    },
+
+    onYearChange(){
+        if (this.selectedDataset){
+            this.mapController?.fetchAndUpdateMap()
+            //prefetch adjacent years if race-ethnicity
+        }
+
+
+    },
+
+    onRaceChange(){
+        if (this.selectedDataset == 'race-ethnicity'){
+            this.mapController?.fetchAndUpdateMap()
+        }
+    },
+
+    onHispanicChange(){
+        if (this.selectedDataset == 'race-ethnicity'){
+            this.mapController?.fetchAndUpdateMap()
+        }
+    },
+
+    onLanguageChange(){
+        if (this.selectedDataset == 'language-proficiency'){
+            this.mapController?.fetchAndUpdateMap()
+        }
+    },
+
+    onMapStyleChange(){
+        this.mapController?.updateMapStyle(this.mapStyle)
+    },
+
+    //ui actions
+    toggleSidebar() {
+        this.sidebarOpen = !this.sidebarOpen;
+    },  
+
+    toggleSettings() {
+        this.settingsOpen = !this.settingsOpen;
+    },
+
+    //getters for computed properties
+    get showDatasetOptions() {
+        return this.selectedDataset !== null;  
+    },
+
+    get showRaceOptions() {
+        return this.selectedDataset === 'race-ethnicity';
+    },
+
+    get showLanguageOptions() {
+        return this.selectedDataset === 'language-proficiency';
+    },
+
+    get timeSliderConfig() {
+        if (this.selectedDataset === 'race-ethnicity') {
+            return { min: 2015, max: 2019, disabled: false}
+
+        } else if (this.selectedDataset === 'language-proficiency') {
+            return { min: 2013, max: 2013, disabled: true}
+        }
+        return { min: 2015, max: 2019, disabled: true};
+
+    },
 
 })
+    
--- components/map.js
+++ components/map.js
@@ -52,12 +52,34 @@ export class MapController {
 
 
     generateCacheKey() {
-        return `${this.app.selectedDataset}_${this.app.geographyLevel}_${this.app.currentYear}_${this.app.selectedRace}_${this.app.selectedHispanic}_${this.app.selectedLanguage}`;
+        return `${this.app.geographyLevel}_${this.app.selectedDataset}_${this.app.currentYear}_${this.app.selectedRace}_${this.app.selectedHispanic}_${this.app.selectedLanguage}`;
     }
 
     async fetchCensusData() {
-        const hispParam = this.app.selectedDataset === 'race-ethnicity' ? `&HISP=${state.selectedHispanic}` : '';
-        const raceParam = this.app.selectedDataset === 'race-ethnicity' ? `&RACE=${state.selectedRace}` : '';
-        const langParam=this.app.selectedDataset==='language-proficiency' ? `&LAN7=${state.selectedLanguage}`: ''
+        const hispParam = this.app.selectedDataset === 'race-ethnicity' ? `&HISP=${this.app.selectedHispanic}` : '';
+        const raceParam = this.app.selectedDataset === 'race-ethnicity' ? `&RACE=${this.app.selectedRace}` : '';
+        const langParam=this.app.selectedDataset==='language-proficiency' ? `&LAN7=${this.app.selectedLanguage}`: ''
+
+        let requestURL;
+        if (this.app.selectedDataset==='race-ethnicity') {
+            const nameField = this.app.currentYear <= 2018 ? 'GEONAME' : 'NAME'
+            const geoLevel=isCountyLevel ? 'county' : 'state'
+            requestURL= `https://api.census.gov/data/${this.app.currentYear}/pep/charagegroups?get=${nameField},POP${raceParam}${hispParam}&for=${geoLevel}:*`
+        } else if (this.app.selectedDataset==='language-proficiency') {
+            const geoLevel=isCountyLevel ? 'county' : 'state'
+            requestURL = `https://api.census.gov/data/2013/language?get=EST,LANLABEL,NAME&for=${geoLevel}:*${langParam}`
+        }
+
+        const response = await fetch(requestURL)
+        console.log("Fetching from:", requestURL)
+        const json = await response.json();
     }
+
+    updateMapStyle(newStyle) {
+        this.countyMap?.updateStyle(newStyle);
+        this.stateMap?.updateStyle(newStyle);
+        lg.updateLegendTheme(newStyle);
+    }
+
+    //prefetch data function
 }
